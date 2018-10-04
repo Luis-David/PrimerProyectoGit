2e3e64ffc4ba2834d32a208657bee64b7e8c2d2c
laboratorio <laboratorio@laboratorio-ThinkCentre-M73>
2018-10-04 13:14:06 -0500

    Segundo commit :)

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -4,6 +4,11 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+/**
+ * 
+ * @author Luis David
+ *
+ */
 public class LibraryTest {
     @Test public void testSomeLibraryMethod() {
         Library classUnderTest = new Library();
